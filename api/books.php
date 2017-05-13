df5e8e94fd2cc0359165664969ad8396e6899df2
wojole <olejarz.wojciech@gmail.com>
2017-05-13 23:03:46 +0200

    added basic layout to index.php and showed book titles on page

--- api/books.php
+++ api/books.php
@@ -3,7 +3,16 @@ require_once __DIR__ . '/src/connect.php';
 require_once __DIR__ . '/src/Book.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $books = Book::loadAllBooks($conn);
-    header('Content-Type: application/json'); //nagłówek informujący że odpowiadamy jsonem
-    echo json_encode($books);
+    if (!isset($_GET['id'])) {
+        $books = Book::loadAllBooks($conn);
+        header('Content-Type: application/json'); //nagłówek informujący że odpowiadamy jsonem
+        echo json_encode($books);
+    }
+    if (isset($_GET['id'])) {
+        $id = trim($_GET['id']);
+        $book = Book::loadFromDB($conn, $id);
+        header('Content-Type: application/json'); //nagłówek informujący że odpowiadamy jsonem
+        echo json_encode($book);
+    }
 }
+
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Książki</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <!-- Zawartość kontenera -->
+    <div class="row">
+        <div class="col-sm-12">
+            <!-- Tytuł -->
+           <h1>Książki</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12" id="books">
+            <!-- Treść -->
+
+        </div>
+    </div>
+</div>
+
+<script
+    src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+<script src="api/app.js" type="text/javascript"></script>
+</body>
+</html>
+<?php
