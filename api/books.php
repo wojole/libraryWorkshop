c572f9c96a836dee342ec793ff567548bbaa3caa
wojole <olejarz.wojciech@gmail.com>
2017-05-14 23:43:24 +0200

    added POST method for adding new book to database

--- api/books.php
+++ api/books.php
@@ -16,4 +16,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         echo json_encode($book->getName()); //wyświetla tylko tytuł
     }
 }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+    if (isset($_POST['name'])&&isset($_POST['author'])) {
+        $name=trim($_POST['name']);
+        $author=trim($_POST['author']);
+
+        $book1=new Book();
+        $success=$book1->create($conn, "$name","$author");
+        if($success===true){
+            echo "Dodano nową książkę do bazy danych";
+        }
+    }
+}
